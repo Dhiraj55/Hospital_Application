b019155f0b61ddc2b999b33b74fa857bb83846e6
Dhiraj Bodake <68703810+Dhiraj55@users.noreply.github.com>
2021-06-05 12:01:18 +0530

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,23 @@
+<?php
+include("header.php");
+?>
+<div class="wrapper col2">
+  <div id="gallery">
+    
+      
+    <div class="clear"></div>
+  </div>
+</div>
+<div class="wrapper col4">
+  <div id="container">
+    <div id="content">
+      
+        <div class="clear"></div>
+     
+    
+    <div class="clear"></div>
+  </div>
+</div>
+<?php
+include("footer.php");
+?>
